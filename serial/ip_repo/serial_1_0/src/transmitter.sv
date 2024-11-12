4946969092249e210dfa850c8aafa857316364b2
Abhishek Dhital <abhishek.dhital@mavs.uta.edu>
2024-11-11 19:41:49 -0600

    Add comments and fix minor issues

--- serial/ip_repo/serial_1_0/src/transmitter.sv
+++ serial/ip_repo/serial_1_0/src/transmitter.sv
@@ -12,7 +12,6 @@ module transmitter(
     output out
 );
 
-   
     reg [4:0] brgen_counter;
     reg baud_en;
     reg out_data;
@@ -71,17 +70,17 @@ module transmitter(
             state <= IDLE;
             out_data <= 1;
         end
-        if (baud_en && enable)
+        if (baud_en && enable)      // if enable is set in the control register and if rising edge of baud rate clock
         begin
             case(state)
                 IDLE:
                 begin
                     out_data <= 1; 
-                    if (!empty)
+                    if (!empty)             // If tx fifo not empty, start the state machine
                     begin
                         state <= START;
                     end
-                    else
+                    else                    // If tx fifo is empty, stay in IDLE state
                     begin
                         state <= IDLE;
                     end
@@ -92,41 +91,41 @@ module transmitter(
                     latched_data[8:0] <= data[8:0];
                     rd_request <= 1;
                     
-                    out_data <= 0;
-                    state <= D0;
+                    out_data <= 0;          // output set to low to signal START bit
+                    state <= D0;            
                 end
                 D0:
-                begin
+                begin                      // output D0-3 without any checks
                     out_data <= latched_data[0];
                     state <= D1;
                 end
                 D1:
                 begin
                     out_data <= latched_data[1];
-                    state <= D1;
+                    state <= D2;
                 end
                 D2:
                 begin
                     out_data <= latched_data[2];
-                    state <= D1;
+                    state <= D3;
                 end
                 D3:
                 begin
                     out_data <= latched_data[3];
-                    state <= D1;
+                    state <= D4;
                 end
                 D4:
                 begin
                     out_data <= latched_data[4];
-                    if (size == 2'b00 && parity != 2'b00)
+                    if (size == 2'b00 && parity != 2'b00)       // if data size is set as 5, and parity is not disabled, next state is P(arity)
                     begin
                         state <= P;
                     end
-                    else if (size == 2'b00 && parity == 2'b00)
+                    else if (size == 2'b00 && parity == 2'b00)  // if data size is set as 5, and parity is disabled, next state is STOP1
                     begin
                         state <= STOP1;
                     end
-                    else
+                    else                                        // in all other cases, next state is D5
                     begin
                         state <= D5;
                     end
@@ -134,15 +133,15 @@ module transmitter(
                 D5:
                 begin
                     out_data <= latched_data[5];
-                    if (size == 2'b01 && parity != 2'b00)
+                    if (size == 2'b01 && parity != 2'b00)       // if data size is set as 6, and parity is not disabled, next state is P(arity)
                     begin
                         state <= P;
                     end
-                    else if (size == 2'b01 && parity == 2'b00)
+                    else if (size == 2'b01 && parity == 2'b00)  // if data size is set as 6, and parity is disabled, next state is STOP1
                     begin
                         state <= STOP1;
                     end
-                    else
+                    else                                        // in all other cases, next state is D6
                     begin
                         state <= D6;
                     end                        
@@ -150,15 +149,15 @@ module transmitter(
                 D6:
                 begin
                     out_data <= latched_data[6];
-                    if (size == 2'b10 && parity != 2'b00)
+                    if (size == 2'b10 && parity != 2'b00)       // if data size is set as 7, and parity is not disabled, next state is P(arity)
                     begin
                         state <= P;
                     end
-                    else if (size == 2'b10 && parity == 2'b00)
+                    else if (size == 2'b10 && parity == 2'b00)  // if data size is set as 7, and parity is disabled, next state is STOP1
                     begin
                         state <= STOP1;
                     end
-                    else
+                    else                                        // in all other cases, next state is D7
                     begin
                         state <= D7;
                     end
@@ -166,11 +165,11 @@ module transmitter(
                 D7:
                 begin
                     out_data <= latched_data[7];
-                    if (parity != 2'b00)
+                    if (parity != 2'b00)                        // if parity is not disabled, next state is P(arity)
                     begin
                         state <= P;    
                     end
-                    else
+                    else                                        // if parity is disabled, next state is STOP1
                     begin
                         state <= STOP1;
                     end
@@ -179,20 +178,21 @@ module transmitter(
                 begin
                     integer n, ones;
                     ones <= 0;
-                    for (n = 0; n < size; n = n + 1)
+                    for (n = 0; n < size; n = n + 1)                // count the number of 1's in the data word
                     begin
                         if (data[n])
                             ones <= ones + 1;
                     end                                       
+                    
                     if (parity == 2'b01)    // even parity
                     begin
-                         out_data <= (ones % 2 == 0) ? 1 : 0;
+                         out_data <= (ones % 2 == 0) ? 1 : 0;       // for even parity, if number of ones is even, parity = 1 else parity = 0
                     end
                     else if (parity == 2'b10)   // odd parity
                     begin
-                        out_data <= (ones % 2 == 1) ? 1 : 0;
+                        out_data <= (ones % 2 == 1) ? 1 : 0;        // for odd parity, if number of ones is odd, parity = 1 else parity = 0
                     end
-                    else
+                    else                                            // if parity = 2'b11, output data[8] bit as parity bit
                     begin
                         out_data <= data[8];
                     end                 
@@ -201,28 +201,27 @@ module transmitter(
                 STOP1:
                 begin
                     out_data <= 1;
-                    rd_request <= 1;
-                    if (stop2 == 0 && !empty)
+                    if (stop2 == 0 && !empty)                       // if stop2 is not enabled, and tx fifo is not empty, go to START
                     begin
                         state <= START;
                     end
-                    else if (stop2 == 0 && empty)
+                    else if (stop2 == 0 && empty)                   // if stop2 is not enabled, and tx fifo is empty, go to IDLE
                     begin
                         state <= IDLE;
                     end               
-                    else
+                    else                                            // in all other cases where stop2 is enabled, regardless of tx fifo being empty or not, go to STOP2
                     begin
                         state <= STOP2;
                     end
                 end    
                 STOP2:
                 begin
-                    out_data <= 1;
-                    if (empty)
+                    out_data <= 1;                                  
+                    if (empty)                                      // if tx fifo is empty, go to IDLE
                     begin
                         state <= IDLE;
                     end
-                    else
+                    else                                            // if tx fifo is not empty, go to START
                     begin
                         state <= START;
                     end
