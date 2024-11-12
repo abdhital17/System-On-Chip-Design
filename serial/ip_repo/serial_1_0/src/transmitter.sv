8a0b850e833873a12cdcd153e170cde0ea2b6759
Abhishek Dhital <abhishek.dhital@mavs.uta.edu>
2024-11-11 19:16:06 -0600

    Complete transmitter module implementation
    - data_request input port changed to output as required
    - FSM states changed from binary to decimal values to prevent errors
    - fifo data is now latched in the START state and next read request is sent
    - All other states programmed as specified to support 5,6,7,8 bit data, parity bit and 1 or 2 bit STOP

--- serial/ip_repo/serial_1_0/src/transmitter.sv
+++ serial/ip_repo/serial_1_0/src/transmitter.sv
@@ -4,34 +4,40 @@ module transmitter(
     input brgen,
     input enable,
     input empty,
-    input size,
+    input [1:0] size,
     input stop2,
     input [1:0] parity,
     input [8:0] data,
-    input data_request,
+    output data_request,
     output out
 );
 
-    reg[4:0] brgen_counter = 0;
-    reg output_en;
+   
+    reg [4:0] brgen_counter;
+    reg baud_en;
     reg out_data;
+    reg rd_request;
+    reg [8:0] latched_data;
+    
+    // assign the output
     assign out = out_data;
+    assign data_request = rd_request;
      
     // FSM for transmitter
     reg [3:0] state;
-    parameter IDLE      = 4'b0000;
-    parameter START     = 4'b0001;
-    parameter D0        = 4'b0010;
-    parameter D1        = 4'b0011;
-    parameter D2        = 4'b0100;
-    parameter D3        = 4'b0101;
-    parameter D4        = 4'b0110;
-    parameter D5        = 4'b0111;
-    parameter D6        = 4'b1000;
-    parameter D7        = 4'b1001;
-    parameter P         = 4'b1010;
-    parameter STOP1     = 4'b1011;
-    parameter STOP2     = 4'b1100;
+    parameter IDLE      = 4'd0;
+    parameter START     = 4'd1;
+    parameter D0        = 4'd2;
+    parameter D1        = 4'd3;
+    parameter D2        = 4'd4;
+    parameter D3        = 4'd5;
+    parameter D4        = 4'd6;
+    parameter D5        = 4'd7;
+    parameter D6        = 4'd8;
+    parameter D7        = 4'd9;
+    parameter P         = 4'd10;
+    parameter STOP1     = 4'd11;
+    parameter STOP2     = 4'd12;
     
     // Block that generates the baud rate by dividing the brgen out by 16
     always_ff @ (posedge(clk))
@@ -39,18 +45,18 @@ module transmitter(
         if (~reset)
         begin
             brgen_counter <= 0;
-            output_en <= 0;
+            baud_en <= 0;
         end
         else if (brgen)
         begin
             brgen_counter <= (brgen_counter + 1) % 17;      // increment brgen_counter modulo 17, so that the max. value it takes is 16
             if (brgen_counter == 16)                        // if brgen_counter reaches 16, enable the fsm
             begin
-                  output_en <= 1;
+                  baud_en <= 1;
             end
             else                                            // when brgen_counter is not 16, disable the fsm
             begin
-                output_en <= 0;
+                baud_en <= 0;
             end
         end
     end
@@ -59,32 +65,171 @@ module transmitter(
     // the transmitter finite state machine which is triggered at the baud rate
     always_ff @ (posedge(clk))
     begin
+        rd_request <= 0;            // clear rd_request on each clock
         if (~reset)
         begin
             state <= IDLE;
+            out_data <= 1;
         end
-        if (output_en)
+        if (baud_en && enable)
         begin
-            if (output_en)
-            begin
-                case(state)
-                    IDLE:
+            case(state)
+                IDLE:
+                begin
+                    out_data <= 1; 
+                    if (!empty)
                     begin
-                        out_data <= 1; 
-                        if (!empty)
-                        begin
-                            state <= START;
-                        end
-                        else
-                        begin
-                            state <= IDLE;
-                        end
+                        state <= START;
                     end
-                    START:
+                    else
                     begin
-                        out_data <= 0;
+                        state <= IDLE;
+                    end
+                end
+                START:
+                begin
+                    // latch the current data and set rd_request to get data for next transmit
+                    latched_data[8:0] <= data[8:0];
+                    rd_request <= 1;
+                    
+                    out_data <= 0;
+                    state <= D0;
+                end
+                D0:
+                begin
+                    out_data <= latched_data[0];
+                    state <= D1;
+                end
+                D1:
+                begin
+                    out_data <= latched_data[1];
+                    state <= D1;
+                end
+                D2:
+                begin
+                    out_data <= latched_data[2];
+                    state <= D1;
+                end
+                D3:
+                begin
+                    out_data <= latched_data[3];
+                    state <= D1;
+                end
+                D4:
+                begin
+                    out_data <= latched_data[4];
+                    if (size == 2'b00 && parity != 2'b00)
+                    begin
+                        state <= P;
+                    end
+                    else if (size == 2'b00 && parity == 2'b00)
+                    begin
+                        state <= STOP1;
+                    end
+                    else
+                    begin
+                        state <= D5;
+                    end
+                end
+                D5:
+                begin
+                    out_data <= latched_data[5];
+                    if (size == 2'b01 && parity != 2'b00)
+                    begin
+                        state <= P;
+                    end
+                    else if (size == 2'b01 && parity == 2'b00)
+                    begin
+                        state <= STOP1;
+                    end
+                    else
+                    begin
+                        state <= D6;
+                    end                        
+                end
+                D6:
+                begin
+                    out_data <= latched_data[6];
+                    if (size == 2'b10 && parity != 2'b00)
+                    begin
+                        state <= P;
+                    end
+                    else if (size == 2'b10 && parity == 2'b00)
+                    begin
+                        state <= STOP1;
+                    end
+                    else
+                    begin
+                        state <= D7;
+                    end
+                end
+                D7:
+                begin
+                    out_data <= latched_data[7];
+                    if (parity != 2'b00)
+                    begin
+                        state <= P;    
+                    end
+                    else
+                    begin
+                        state <= STOP1;
+                    end
+                end
+                P:
+                begin
+                    integer n, ones;
+                    ones <= 0;
+                    for (n = 0; n < size; n = n + 1)
+                    begin
+                        if (data[n])
+                            ones <= ones + 1;
+                    end                                       
+                    if (parity == 2'b01)    // even parity
+                    begin
+                         out_data <= (ones % 2 == 0) ? 1 : 0;
+                    end
+                    else if (parity == 2'b10)   // odd parity
+                    begin
+                        out_data <= (ones % 2 == 1) ? 1 : 0;
+                    end
+                    else
+                    begin
+                        out_data <= data[8];
                     end                 
-            end
+                    state <= STOP1;
+                end           
+                STOP1:
+                begin
+                    out_data <= 1;
+                    rd_request <= 1;
+                    if (stop2 == 0 && !empty)
+                    begin
+                        state <= START;
+                    end
+                    else if (stop2 == 0 && empty)
+                    begin
+                        state <= IDLE;
+                    end               
+                    else
+                    begin
+                        state <= STOP2;
+                    end
+                end    
+                STOP2:
+                begin
+                    out_data <= 1;
+                    if (empty)
+                    begin
+                        state <= IDLE;
+                    end
+                    else
+                    begin
+                        state <= START;
+                    end
+                end
+                default:
+                    state <= IDLE;                                                                                                                                                                                     
+            endcase               
         end
     end
 endmodule
